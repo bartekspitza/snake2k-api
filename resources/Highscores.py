7571572523259374ad5023cae5201498155f648e
bartekspitza <bartekspitza@hotmail.com>
2018-10-26 22:20:47 +0200

    added resources to api

--- resources/Highscores.py
+++ resources/Highscores.py
@@ -1,8 +1,15 @@
+from flask import jsonify
 from flask_restful import Resource
+import sqlite3
+import os
 
 class Highscores(Resource):
 	def get(self):
-		highscore = {"nickname": "bartek", "score": "100"}
+		path = os.getenv('DB_PATH', os.environ['TMP_PATH'])
+		conn = sqlite3.connect(path)
+		c = conn.cursor()
 
+		c.execute("SELECT * FROM highscores")
+		highscores = [{"nickname": x[0], "score": x[1]} for x in c.fetchall()]
 
-		return highscore
+		return highscores
--- resources/NewHighscore.py
+++ resources/NewHighscore.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
+import sqlite3
 
 class NewHighscore(Resource):
 	def post(self):
-		
 		return {"nickname": "bartek", "score": "2200"}
